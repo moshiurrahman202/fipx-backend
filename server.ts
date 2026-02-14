f8ebb355ac5c32042b7ea7771061a5d31b70be5d
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2026-02-14 19:47:15 +0600

    firebase setupe

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/db";
+import connectDB from "./src/config/db";
+import authMiddleware from "./middleware/auth.middleware";
 
 dotenv.config();
 
@@ -15,7 +16,11 @@ app.use(express.json());
 app.get("/", (_req, res) => {
   res.send("ZipX Backend Running 🚀");
 });
-
+app.get("/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "You are authenticated 🚀",
+  });
+});
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
